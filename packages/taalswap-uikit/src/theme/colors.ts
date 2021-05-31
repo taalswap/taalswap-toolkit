52fc06a46f7648e2f3b25ca9134a665634bbc9ab
helloljho <helloljho@gmail.com>
2021-05-31 17:48:58 +0900

    Update colors.ts color modify

--- packages/taalswap-uikit/src/theme/colors.ts
+++ packages/taalswap-uikit/src/theme/colors.ts
@@ -3,9 +3,9 @@ import { Colors } from "./types";
 export const baseColors = {
   failure: "rgb(255, 72, 66)",
   primary: "#00ab55",
-  primaryBright: "#53DEE9",
+  primaryBright: "#00ab55",
   primaryDark: "#0098A1",
-  secondary: "#7645D9",
+  secondary: "#00ab55",
   success: "#00ab55",
   warning: "rgb(255, 72, 66)",
 };
@@ -27,7 +27,7 @@ export const lightColors: Colors = {
   dropdownDeep: "#EEEEEE",
   invertedContrast: "#FFFFFF",
   input: "#f3f5f7",
-  inputSecondary: "#d7caec",
+  inputSecondary: "#00ab55",
   tertiary: "#F3F5F7",
   text: "#212b36",
   textDisabled: "#BDC2C4",
@@ -36,7 +36,7 @@ export const lightColors: Colors = {
   gradients: {
     bubblegum: "linear-gradient(139.73deg, #E5FDFF 0%, #F3EFFF 100%)",
     inverseBubblegum: "linear-gradient(139.73deg, #F3EFFF 0%, #E5FDFF 100%)",
-    cardHeader: "linear-gradient(111.68deg, #F2ECF2 0%, #E8F2F6 100%)",
+    cardHeader: "linear-gradient(111.68deg, #F4F6F8 0%, #F4F6F8 100%)",
     blue: "linear-gradient(180deg, #A7E8F1 0%, #94E1F2 100%)",
     violet: "linear-gradient(180deg, #E2C9FB 0%, #CDB8FA 100%)",
     violetAlt: "linear-gradient(180deg, #CBD7EF 0%, #9A9FD0 100%)",
@@ -47,7 +47,7 @@ export const lightColors: Colors = {
 export const darkColors: Colors = {
   ...baseColors,
   ...additionalColors,
-  secondary: "#9A6AFF",
+  secondary: "#00ab55",
   background: "#161C24",
   backgroundDisabled: "rgb(33, 43, 54)",
   backgroundAlt: "rgb(33, 43, 54)",
@@ -67,7 +67,7 @@ export const darkColors: Colors = {
   gradients: {
     bubblegum: "linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)",
     inverseBubblegum: "linear-gradient(139.73deg, #3D2A54 0%, #313D5C 100%)",
-    cardHeader: "linear-gradient(166.77deg, #3B4155 0%, #3A3045 100%)",
+    cardHeader: "linear-gradient(166.77deg, rgb(33,43,54) 0%, rgb(33,43,54) 100%)",
     blue: "linear-gradient(180deg, #00707F 0%, #19778C 100%)",
     violet: "linear-gradient(180deg, #6C4999 0%, #6D4DB2 100%)",
     violetAlt: "linear-gradient(180deg, #434575 0%, #66578D 100%)",
