42c4e456f0d071bd1d72db5aebb2fa7641674081
Peter Nahm <hwnahm@gmail.com>
2021-06-18 20:04:15 +0900

    Change Scanner link

--- packages/taalswap-uikit/src/widgets/WalletModal/AccountModal.tsx
+++ packages/taalswap-uikit/src/widgets/WalletModal/AccountModal.tsx
@@ -23,7 +23,7 @@ const AccountModal: React.FC<Props> = ({ account, logout, onDismiss = () => null
       {account}
     </Text>
     <Flex mb="32px">
-      <LinkExternal small href={`https://bscscan.com/address/${account}`} mr="16px">
+      <LinkExternal small href={`https://rinkeby.etherscan.io/address/${account}`} mr="16px">
         View on EtherScan
       </LinkExternal>
       <CopyToClipboard toCopy={account}>Copy Address</CopyToClipboard>
