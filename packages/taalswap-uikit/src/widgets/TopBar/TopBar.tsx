57b8db5f3773eed0407e7d914da677fc7e0ab047
helloljho <helloljho@gmail.com>
2021-06-18 15:04:26 +0900

    change landing topbar

--- packages/taalswap-uikit/src/widgets/TopBar/TopBar.tsx
+++ packages/taalswap-uikit/src/widgets/TopBar/TopBar.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Link from "../../components/Link/Link";
 import TopMenu from "./TopMenu";
 import logo_img from "./images/TAAL_Logo.png";
+import logo_imgChange from "./images/TAAL_Logo.svg";
 import Button from "../../components/Button/Button";
 import { useWalletModal } from "../WalletModal";
 import { Login } from "../WalletModal/types";
@@ -15,74 +16,61 @@ interface Props {
 const TopBar: React.FC<Props> = ({ account, login, logout }) => {
   const { onPresentConnectModal, onPresentAccountModal } = useWalletModal(login, logout, account);
   const accountEllipsis = account ? `${account.substring(0, 4)}...${account.substring(account.length - 4)}` : null;
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const updateScroll = () => {
+    setScrollPosition(window.scrollY || document.documentElement.scrollTop);
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", updateScroll);
+  });
 
   return (
-    <div className="tabbar_wrap">
-      <div>
-        <Link href="http://localhost:3001">
-          <img src={logo_img} alt="logo_img" className="top_logo" />
-        </Link>
-      </div>
-      <div className="top_menu">
+    <div className={scrollPosition < 50 ? "original_header" : "change_header"}>
+      <div className="tabbar_wrap">
         <div>
-          <Link href="http://localhost:3001" style={{ color: "#212b36", textDecoration: "none" }}>
-            Home
+          <Link href="/">
+            <img src={logo_img} alt="logo_img" className="top_logo" />
+            <img src={logo_imgChange} alt="logo_img" className="top_logochange" />
           </Link>
         </div>
-        <div style={{ marginLeft: "30px" }}>
-          <Link href="http://localhost:3000/#/swap" style={{ color: "#212b36", textDecoration: "none" }}>
-            Swap
-          </Link>
+        <div className="top_menu">
+          <div>
+            <Link href="/" style={{ color: "#00ab55", textDecoration: "none", fontSize: "14px" }}>
+              Home
+            </Link>
+          </div>
+          <div style={{ marginLeft: "30px" }}>
+            <Link href="http://localhost:3000/#/swap" style={{ textDecoration: "none", fontSize: "14px" }}>
+              Swap
+            </Link>
+          </div>
+          <div style={{ marginLeft: "30px" }}>
+            <Link href="http://localhost:3000/#/pool" style={{ textDecoration: "none", fontSize: "14px" }}>
+              Liquidity
+            </Link>
+          </div>
+          <div style={{ marginLeft: "30px" }}>
+            <Link href="/farms" style={{ textDecoration: "none", fontSize: "14px" }}>
+              Farms
+            </Link>
+          </div>
+          <div style={{ marginLeft: "30px" }}>
+            <Link href="/pools" style={{ textDecoration: "none", fontSize: "14px" }}>
+              Staking
+            </Link>
+          </div>
+          <div style={{ marginLeft: "30px" }}>
+            <Link href="/" style={{ textDecoration: "none", fontSize: "14px" }}>
+              IDO
+            </Link>
+          </div>
+          <div>
+            <input type="button" value="Connect Wallet" className="connect_btn" />
+          </div>
         </div>
-        <div style={{ marginLeft: "30px" }}>
-          <Link href="http://localhost:3000/#/pool" style={{ color: "#212b36", textDecoration: "none" }}>
-            Liquidity
-          </Link>
+        <div className="mobile_menu" style={{ cursor: "pointer" }}>
+          <TopMenu />
         </div>
-        <div style={{ marginLeft: "30px" }}>
-          <Link href="http://localhost:3001/farms" style={{ color: "#212b36", textDecoration: "none" }}>
-            Farms
-          </Link>
-        </div>
-        <div style={{ marginLeft: "30px" }}>
-          <Link href="http://localhost:3001/pools" style={{ color: "#212b36", textDecoration: "none" }}>
-            Staking
-          </Link>
-        </div>
-        <div style={{ marginLeft: "30px" }}>
-          <Link href="https://taalswap.finance" target="_blank" style={{ color: "#212b36", textDecoration: "none" }}>
-            IDO
-          </Link>
-        </div>
-        <div style={{ marginLeft: "30px" }}>
-          <Link
-            href="https://taalswap.gitbook.io/taalswap-docs-v-2-0"
-            target="_blank"
-            style={{ color: "#212b36", textDecoration: "none" }}
-          >
-            Docs
-          </Link>
-        </div>
-        {/* <div>
-          <input type="button" value="Connect Wallet" className="connect_btn" />
-        </div> */}
-        <div>
-          {!account && (
-            <Button
-              scale="sm"
-              onClick={() => {
-                onPresentConnectModal();
-              }}
-              style={{ backgroundColor: "#00ab55" }}
-              className="connect_btn"
-            >
-              Connect Wallet
-            </Button>
-          )}
-        </div>
-      </div>
-      <div className="mobile_menu" style={{ cursor: "pointer" }}>
-        <TopMenu />
       </div>
     </div>
   );
