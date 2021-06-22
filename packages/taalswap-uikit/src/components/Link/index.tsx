d4c7ebc6a98b9820552d6b52d89a450bfdc3eb9b
Peter Nahm <hwnahm@gmail.com>
2021-06-22 17:11:30 +0900

    Add StyledLink with no Icon

--- packages/taalswap-uikit/src/components/Link/index.tsx
+++ packages/taalswap-uikit/src/components/Link/index.tsx
@@ -1,3 +1,4 @@
 export { default as Link } from "./Link";
 export { default as LinkExternal } from "./LinkExternal";
+export { default as LinkExternalNoIcon } from "./LinkExternalNoIcon";
 export type { LinkProps } from "./types";
