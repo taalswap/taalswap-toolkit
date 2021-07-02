6716f48a3ef9e3a09be94a070f64351adb7b4346
Peter Nahm <hwnahm@gmail.com>
2021-07-02 23:09:56 +0900

    Update fot Ropsten

--- packages/taalswap-uikit/src/widgets/WalletModal/AccountModal.tsx
+++ packages/taalswap-uikit/src/widgets/WalletModal/AccountModal.tsx
@@ -27,7 +27,7 @@ const AccountModal: React.FC<Props> = ({ account, logout, onDismiss = () => null
       {account}
     </Text>
     <Flex mb="32px">
-      <LinkExternal small href={`https://rinkeby.etherscan.io/address/${account}`} mr="16px">
+      <LinkExternal small href={`https://ropsten.etherscan.io/address/${account}`} mr="16px">
         View on EtherScan
       </LinkExternal>
       <CopyToClipboard toCopy={account}>Copy Address</CopyToClipboard>
