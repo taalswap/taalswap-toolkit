366d9d0dadc73fa9faf2b1c100f5a599cafec6b0
helloljho <helloljho@gmail.com>
2021-09-14 13:48:09 +0900

    add: talken connect button

--- packages/taalswap-uikit/src/widgets/WalletModal/ConnectModal.tsx
+++ packages/taalswap-uikit/src/widgets/WalletModal/ConnectModal.tsx
@@ -5,8 +5,10 @@ import { HelpIcon } from "../../components/Svg";
 import { Modal } from "../Modal";
 import WalletCard from "./WalletCard";
 import config from "./config";
-import { Login } from "./types";
-import CloseBtn from "./icons/closeBtn"
+import { Config, Login, ConnectorNames } from "./types";
+import CloseBtn from "./icons/closeBtn";
+import { useMatchBreakpoints } from "../../hooks";
+import Talken from "./icons/Talken";
 
 interface Props {
   login: Login;
@@ -21,25 +23,38 @@ const HelpLink = styled(Link)`
   color: #637381;
 `;
 
-const ConnectModal: React.FC<Props> = ({ login, onDismiss = () => null }) => (
-  <Modal title="CONNECT TO A WALLET" onDismiss={onDismiss} style={{position:'relative'}}>
-    <div style={{position:"absolute",right:"20px",top:'20px',cursor:'pointer'}}>
-      <CloseBtn onClick={onDismiss}/>
-    </div>
-    {config.map((entry, index) => (
-      <WalletCard
-        key={entry.title}
-        login={login}
-        walletConfig={entry}
-        onDismiss={onDismiss}
-        mb={index < config.length - 1 ? "8px" : "0"}
-      />
-    ))}
-    {/* <HelpLink href="https://docs.pancakeswap.finance/help/faq#how-do-i-connect-my-wallet-to-pancakeswap" external>
+const talkenConnector: Config = {
+  title: "Talken",
+  icon: Talken,
+  connectorId: ConnectorNames.Injected,
+};
+
+const ConnectModal: React.FC<Props> = ({ login, onDismiss = () => null }) => {
+  const { isXs, isSm, isMd, isLg, isXl } = useMatchBreakpoints();
+
+  return (
+    <Modal title="CONNECT TO A WALLET" onDismiss={onDismiss} style={{ position: "relative" }}>
+      <div style={{ position: "absolute", right: "20px", top: "20px", cursor: "pointer" }}>
+        <CloseBtn onClick={onDismiss} />
+      </div>
+      {(isXs || isSm) && (
+        <WalletCard key="talken" login={login} walletConfig={talkenConnector} onDismiss={onDismiss} mb="8px" />
+      )}
+      {config.map((entry, index) => (
+        <WalletCard
+          key={entry.title}
+          login={login}
+          walletConfig={entry}
+          onDismiss={onDismiss}
+          mb={index < config.length - 1 ? "8px" : "0"}
+        />
+      ))}
+      {/* <HelpLink href="https://docs.pancakeswap.finance/help/faq#how-do-i-connect-my-wallet-to-pancakeswap" external>
     <HelpIcon color="#637381" mr="6px" />
       Learn how to connect
     </HelpLink> */}
-  </Modal>
-);
+    </Modal>
+  );
+};
 
 export default ConnectModal;
