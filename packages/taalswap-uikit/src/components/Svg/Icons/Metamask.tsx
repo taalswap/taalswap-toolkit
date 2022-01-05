3da2ea15b58634f80aea9773ddfb347abc936009
helloljho <helloljho@gmail.com>
2022-01-05 10:53:27 +0900

    fix : svg warning

--- packages/taalswap-uikit/src/components/Svg/Icons/Metamask.tsx
+++ packages/taalswap-uikit/src/components/Svg/Icons/Metamask.tsx
@@ -6,16 +6,16 @@ const Icon: React.FC<SvgProps> = (props) => {
   return (
     <Svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 26 26" {...props}>
       <g id="그룹_936" data-name="그룹 936" transform="translate(-53.061 -281.426)">
-      <path id="패스_214" data-name="패스 214" d="M60.253,287.932l4.564,3.726L62.762,293.5l-2.959.864" fill="none" stroke="#00ab55" stroke-miterlimit="10" stroke-width="1"/>
-      <path id="패스_215" data-name="패스 215" d="M72.465,287.834l-5.454,3.8,2.008,1.891,2.976.829" fill="none" stroke="#00ab55" stroke-miterlimit="10" stroke-width="1"/>
-      <path id="패스_216" data-name="패스 216" d="M59.91,287.77l4.318,2.031h3.747l4.331-2.031.747,2.171-.374,2.265.374.2-.455.339.291.245-.373.408.257.222-.584.724,1.109,3.21-.782,3.338-3.3-1.272s-.654.513-2.919.432c0,0-2.369.2-3.256-.444l-3.432,1.331-.782-3.385,1.173-3.2-.621-.771.271-.192-.412-.421.333-.236-.411-.368.315-.2-.359-2.346Z" fill="none" stroke="#00ab55" stroke-miterlimit="10" stroke-width="1.5"/>
+      <path id="패스_214" data-name="패스 214" d="M60.253,287.932l4.564,3.726L62.762,293.5l-2.959.864" fill="none" stroke="#00ab55" strokeMiterlimit="10" strokeWidth="1"/>
+      <path id="패스_215" data-name="패스 215" d="M72.465,287.834l-5.454,3.8,2.008,1.891,2.976.829" fill="none" stroke="#00ab55" strokeMiterlimit="10" strokeWidth="1"/>
+      <path id="패스_216" data-name="패스 216" d="M59.91,287.77l4.318,2.031h3.747l4.331-2.031.747,2.171-.374,2.265.374.2-.455.339.291.245-.373.408.257.222-.584.724,1.109,3.21-.782,3.338-3.3-1.272s-.654.513-2.919.432c0,0-2.369.2-3.256-.444l-3.432,1.331-.782-3.385,1.173-3.2-.621-.771.271-.192-.412-.421.333-.236-.411-.368.315-.2-.359-2.346Z" fill="none" stroke="#00ab55" strokeMiterlimit="10" strokeWidth="1.5"/>
       <path id="패스_217" data-name="패스 217" d="M64.393,295.162l-1.33.557,1.866.433Z" fill="#00ab55"/>
       <path id="패스_218" data-name="패스 218" d="M67.114,296.151l1.866-.442-1.316-.647Z" fill="#00ab55"/>
       <path id="패스_219" data-name="패스 219" d="M64.816,298.8l.137-1.313.248-.248,1.7-.014.233.278.155,1.3-.222-.233-1.984.012Z" fill="#00ab55"/>
       
       <g id="타원_24" data-name="타원 24">
         <circle id="타원_30" data-name="타원 30" cx="12" cy="12" r="12" transform="translate(54.061 282.426)" fill="none"/>
-        <circle id="타원_31" data-name="타원 31" cx="12" cy="12" r="12" transform="translate(54.061 282.426)" fill="none" stroke="#00ab55" stroke-width="2"/>
+        <circle id="타원_31" data-name="타원 31" cx="12" cy="12" r="12" transform="translate(54.061 282.426)" fill="none" stroke="#00ab55" strokeWidth="2"/>
       </g>
       </g>
     </Svg>
